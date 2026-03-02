b522c213b3a78e4c474bb3bb04d037a09cc3a9c9
User <user@example.com>
2026-03-02 20:39:50 +0800

    Add alias for index/index path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -7,7 +8,15 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': './'
+      '@': path.resolve(__dirname, './'),
+      'index/index': path.resolve(__dirname, './pages/home/index.vue')
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
     }
   }
 })
